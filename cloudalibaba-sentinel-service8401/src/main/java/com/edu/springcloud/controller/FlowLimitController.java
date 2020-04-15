d5d3d664da7bdc76f4c3f5222dbb0a24fe239c6b
wis-w <1044957202@qq.com>
2020-04-15 21:54:34 +0800

    [2020-04-15]sentinal控制台服务降级验证

--- cloudalibaba-sentinel-service8401/src/main/java/com/edu/springcloud/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/edu/springcloud/controller/FlowLimitController.java
@@ -4,8 +4,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.concurrent.TimeUnit;
-
 /**
  * @author wyg_edu
  * @version 1.0
@@ -27,11 +25,12 @@ public class FlowLimitController {
 
     @GetMapping("/testD")
     public String testD(){
-        try {
-            TimeUnit.SECONDS.sleep(1);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+//        try {
+//            TimeUnit.SECONDS.sleep(1);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+        int a = 10 / 0;
         log.info("testD");
         return "___testD";
     }
