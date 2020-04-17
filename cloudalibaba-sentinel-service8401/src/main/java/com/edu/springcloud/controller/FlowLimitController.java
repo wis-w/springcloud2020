92bcbf4445ae6d0d728bcdd7bc4df65d196f8881
wis-w <1044957202@qq.com>
2020-04-17 21:05:12 +0800

    [2020-04-17]sentinal限流方案：热度限流

--- cloudalibaba-sentinel-service8401/src/main/java/com/edu/springcloud/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/edu/springcloud/controller/FlowLimitController.java
@@ -1,7 +1,10 @@
 package com.edu.springcloud.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -29,9 +32,23 @@ public class FlowLimitController {
 //            TimeUnit.SECONDS.sleep(1);
 //        } catch (InterruptedException e) {
 //            e.printStackTrace();
-//        }
+//         }
         int a = 10 / 0;
         log.info("testD");
         return "___testD";
+    }
+
+    /**
+     * blockHandler该方案只对限流起作用，不包括运行异常
+     */
+    @GetMapping("/testHotKey")
+    @SentinelResource(value = "testHotKey", blockHandler = "dealTestHotKey")// 开启热点限流 value可以为任意 并指定方案
+    public String testHotKey(@RequestParam(value = "p1", required = false) String p1,
+                             @RequestParam(value = "p2", required = false) String p2) {
+        return "=--= HOSTKEY-";
+    }
+
+    public String dealTestHotKey(String p1, String p2, BlockException exception) {// 参数列表
+        return "=--= HOSTKEY=--=error";
     }
 }
