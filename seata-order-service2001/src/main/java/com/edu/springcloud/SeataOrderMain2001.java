6a04b115793ce57c2f64d618be53048cfdeee1d6
wis-w <1044957202@qq.com>
2020-04-18 22:07:20 +0800

    [2020-04-18]seata新建订单模块

--- seata-order-service2001/src/main/java/com/edu/springcloud/SeataOrderMain2001.java
+++ seata-order-service2001/src/main/java/com/edu/springcloud/SeataOrderMain2001.java
@@ -2,7 +2,6 @@ package com.edu.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
@@ -11,9 +10,10 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
  * @version 1.0
  * @date 2020/4/18 18:06
  */
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)// 取消数据源的自动配置
+//@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)// 取消数据源的自动配置
 @EnableFeignClients
 @EnableDiscoveryClient
+@SpringBootApplication
 public class SeataOrderMain2001 {
     public static void main(String[] args) {
         SpringApplication.run(SeataOrderMain2001.class, args);
